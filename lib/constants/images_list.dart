410706584065e3118a5595eb99021e3d1e771bdc
Mohammad Asim Jan <mohammadasimjan@gmail.com>
2024-09-14 21:42:54 +0500

    constants

---
+++ lib/constants/images_list.dart
@@ -0,0 +1,16 @@
+class Images {
+  List<String> servicesImagesList = [];
+
+  static List<String> serviceImages() {
+    return [
+      'Nails',
+      'Hair',
+      'Facial',
+      'Massage',
+      'Waxing',
+      'Threading',
+      'Nultralucious 7D hifu',
+      'Lucious Slim Rf cavitation.png',
+    ];
+  }
+}
